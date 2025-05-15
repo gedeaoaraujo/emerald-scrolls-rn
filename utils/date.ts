2e33a5e7f5f475c3414782b364fd018821fa9275
gedeoaraujo <gedeao.lemobs@gmail.com>
2025-05-15 19:10:52 -0300

    Creating date function util.

---
+++ utils/date.ts
@@ -0,0 +1,14 @@
+export const dateTimeNow = (): string => {
+  const options: Intl.DateTimeFormatOptions = {
+    year: 'numeric',
+    month: '2-digit',
+    day: '2-digit',
+    hour: '2-digit',
+    minute: '2-digit',
+    hour12: false,
+  };
+
+  return new Intl
+    .DateTimeFormat('pt-BR', options)
+    .format(new Date());
+}
