b2fde7306d92ffc85b4daf228fd7bcdbd0cc398e
gedeoaraujo <gedeao.lemobs@gmail.com>
2025-06-06 21:01:33 -0300

    Creating a mocked list of scrolls

---
+++ data/MockedScrolls.ts
@@ -0,0 +1,20 @@
+export const scrollsList = [
+    {
+        id: 1,
+        title: 'Title 1',
+        date: '05/02/25 14:00',
+        text: 'Something here...'
+    },
+    {
+        id: 2,
+        title: 'Title 2',
+        date: '05/02/25 14:00',
+        text: 'Something here...'
+    },
+    {
+        id: 3,
+        title: 'Title 3',
+        date: '05/02/25 14:00',
+        text: 'Something here...'
+    },
+]
