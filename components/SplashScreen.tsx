355aa25e3a94e623789f01ba319c52849d10a615
gedeoaraujo <gedeao.lemobs@gmail.com>
2025-06-09 19:34:32 -0300

    Adding biometry check on the button

--- components/SplashScreen.tsx
+++ components/SplashScreen.tsx
@@ -1,8 +1,6 @@
 import { Colors } from '../colors';
-import { View, Text, StyleSheet } from 'react-native';
 import { FontAwesome6 } from '@expo/vector-icons';
 import { checkBiometry } from '../utils/biometry';
-import { useEffect } from 'react';
 import { PasswordKeyboard } from './PasswordKeyboard';
 import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
 
@@ -14,16 +12,20 @@ const LogoFragment = () => (
 )
 
 export const SplashScreen = ({ navigation }) => {
-  useEffect(() => {
+  const callBiometry = () => {
     checkBiometry(() => {
       navigation.navigate('Home')
     })
-  }, [])
-
+  }
+ 
   return (
     <View style={styles.container}>
       <LogoFragment />
       <PasswordKeyboard />
+      <TouchableOpacity
+        onPress={callBiometry}>
+        <FontAwesome6 name='fingerprint' size={50} color='white'/>
+      </TouchableOpacity>
     </View>
   )
 }
