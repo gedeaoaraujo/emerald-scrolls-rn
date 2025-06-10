a23079145a940b82e6a41d540d4b24f133a1f187
gedeoaraujo <gedeao.lemobs@gmail.com>
2025-06-10 02:13:10 -0300

    Using context functions

--- components/PasswordKeyboard.tsx
+++ components/PasswordKeyboard.tsx
@@ -1,10 +1,11 @@
-import { useState } from "react"
+import { useContext } from "react"
 import { Colors } from "../colors"
-import { View, Text, StyleSheet, TextInput } from "react-native"
 import { FloatActionBtn } from "./FloatActionBtn"
+import { PasswordContext } from "../contexts/PasswordContext"
+import { View, Text, StyleSheet, TextInput } from "react-native"
 
 export const PasswordKeyboard = () => {
-  const [password, setPassword] = useState('')
+  const { password, onChangePassword } = useContext(PasswordContext)
 
   return (
     <View style={styles.container}>
@@ -14,30 +15,30 @@ export const PasswordKeyboard = () => {
         value={password}
         secureTextEntry={true}
         style={styles.password}
-        onChangeText={setPassword}
+        onChangeText={onChangePassword}
       />
       <View style={styles.row}>
-        <FloatActionBtn text='1' onClick={()=>setPassword(password+1)} />
-        <FloatActionBtn text='2' onClick={()=>setPassword(password+2)} />
-        <FloatActionBtn text='3' onClick={()=>setPassword(password+3)} />
+        <FloatActionBtn text='1' onClick={()=>onChangePassword(password+1)} />
+        <FloatActionBtn text='2' onClick={()=>onChangePassword(password+2)} />
+        <FloatActionBtn text='3' onClick={()=>onChangePassword(password+3)} />
       </View>
       <View style={styles.row}>
-        <FloatActionBtn text='4' onClick={()=>setPassword(password+4)} />
-        <FloatActionBtn text='5' onClick={()=>setPassword(password+5)} />
-        <FloatActionBtn text='6' onClick={()=>setPassword(password+6)} />
+        <FloatActionBtn text='4' onClick={()=>onChangePassword(password+4)} />
+        <FloatActionBtn text='5' onClick={()=>onChangePassword(password+5)} />
+        <FloatActionBtn text='6' onClick={()=>onChangePassword(password+6)} />
       </View>
       <View style={styles.row}>
-        <FloatActionBtn text='7' onClick={()=>setPassword(password+7)} />
-        <FloatActionBtn text='8' onClick={()=>setPassword(password+8)} />
-        <FloatActionBtn text='9' onClick={()=>setPassword(password+9)} />
+        <FloatActionBtn text='7' onClick={()=>onChangePassword(password+7)} />
+        <FloatActionBtn text='8' onClick={()=>onChangePassword(password+8)} />
+        <FloatActionBtn text='9' onClick={()=>onChangePassword(password+9)} />
       </View>
       <View style={styles.row}>
-        <FloatActionBtn text='#' onClick={()=>setPassword(password+'#')} />
-        <FloatActionBtn text='0' onClick={()=>setPassword(password+0)} />
+        <FloatActionBtn text='#' onClick={()=>onChangePassword(password+'#')} />
+        <FloatActionBtn text='0' onClick={()=>onChangePassword(password+0)} />
         <FloatActionBtn
           icon='delete-left'
           onClick={()=>{
-            setPassword(password.substring(0, password.length-1))}
+            onChangePassword(password.substring(0, password.length-1))}
           }
         />
       </View>
