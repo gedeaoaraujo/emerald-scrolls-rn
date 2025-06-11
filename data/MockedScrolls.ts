15eb17693abe639ae938b3fe95ea3902108207d6
gedeoaraujo <gedeao.lemobs@gmail.com>
2025-06-11 17:46:21 -0300

    Updating mocks

--- data/MockedScrolls.ts
+++ data/MockedScrolls.ts
@@ -2,19 +2,19 @@ export const scrollsList = [
     {
         id: 1,
         title: 'Title 1',
-        date: '05/02/25 14:00',
+        date: new Date().toISOString(),
         text: 'Something here...'
     },
     {
         id: 2,
         title: 'Title 2',
-        date: '05/02/25 14:00',
+        date: new Date().toISOString(),
         text: 'Something here...'
     },
     {
         id: 3,
         title: 'Title 3',
-        date: '05/02/25 14:00',
+        date: new Date().toISOString(),
         text: 'Something here...'
     },
 ]
