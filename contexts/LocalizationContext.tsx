9459b4e9d8b626a93f1b2b8ae0581f18e6562705
gedeoaraujo <gedeao.lemobs@gmail.com>
2025-06-23 12:34:29 -0300

    Removing language toggle

--- contexts/LocalizationContext.tsx
+++ contexts/LocalizationContext.tsx
@@ -3,7 +3,6 @@ import { useTranslation } from 'react-i18next';
 
 export const LocalizationContext = createContext({
   language: 'enUS',
-  toggleLang: ()=>{},
   selectLang: (lang: string)=>{},
 })
 
@@ -11,18 +10,13 @@ export function LocalizationProvider({ children }) {
   const { i18n } = useTranslation()
   const [language, setLanguage] = useState('enUS')
 
-  const toggleLang = () => {
-    setLanguage(language == 'ptBR' ? 'enUS' : 'ptBR')
-    i18n.changeLanguage(language)
-  }
-
   const selectLang = (lang: string) => {
     setLanguage(lang)
     i18n.changeLanguage(lang)
   }
 
   return (
-    <LocalizationContext value={{ language, toggleLang, selectLang }}>
+    <LocalizationContext value={{ language, selectLang }}>
       {children}
     </LocalizationContext>
   )
