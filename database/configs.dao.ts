5d7f9efe65c35de8b73340866a455931bef42577
gedeoaraujo <gedeao.lemobs@gmail.com>
2025-06-24 16:12:10 -0300

    Creating database configs queries

---
+++ database/configs.dao.ts
@@ -0,0 +1,51 @@
+import * as SQLite from 'expo-sqlite'
+
+export async function getThemeConfig(): Promise<string|null> {
+  try {
+    const db = await SQLite.openDatabaseAsync('emerald.db')
+    const res = await db.getFirstAsync<{ value: string }>(
+      'SELECT value FROM configs WHERE id = 1'
+    )
+    return res?.value ?? null
+  } catch (error) {
+    console.log('Error: ', error);
+    throw error;
+  }
+}
+
+export async function setThemeConfig(value: string) {
+  try {
+    const db = await SQLite.openDatabaseAsync('emerald.db')
+    await db.runAsync(
+      "UPDATE configs SET value = ? WHERE key = 'theme'", value
+    )
+  } catch (error) {
+    console.log('Error: ', error);
+    throw error;
+  }
+}
+
+export async function getLanguageConfig(): Promise<string|null> {
+  try {
+    const db = await SQLite.openDatabaseAsync('emerald.db')
+    const res = await db.getFirstAsync<{ value: string }>(
+      'SELECT value FROM configs WHERE id = 2'
+    )
+    return res?.value ?? null
+  } catch (error) {
+    console.log('Error: ', error);
+    throw error;
+  }
+}
+
+export async function setLanguageConfig(value: string) {
+  try {
+    const db = await SQLite.openDatabaseAsync('emerald.db')
+    await db.runAsync(
+      "UPDATE configs SET value = ? WHERE key = 'language'", value
+    )
+  } catch (error) {
+    console.log('Error: ', error);
+    throw error;
+  }
+}
