457c373c359831cac87b12916d3d87049a6df327
gedeoaraujo <gedeao.lemobs@gmail.com>
2025-06-24 20:12:01 -0300

    Adjusting textinput offset

--- components/EditScrollScreen.tsx
+++ components/EditScrollScreen.tsx
@@ -1,9 +1,12 @@
-import { useContext, useEffect } from 'react';
-import { StyleSheet, TextInput, View } from 'react-native';
+import { useContext, useEffect, useState } from 'react';
 import { ScrollsContext } from '../contexts/ScrollsContext';
 import { useTranslation } from 'react-i18next'
 import { DateHeader } from './DateHeader';
 import { useTheme } from '../theme/ThemeContext';
+import { 
+  Keyboard, KeyboardAvoidingView, Platform, 
+  SafeAreaView, StyleSheet, TextInput, View 
+} from 'react-native';
 
 export const EditScrollScreen = ({ route }) => {
   const {
@@ -14,16 +17,32 @@ export const EditScrollScreen = ({ route }) => {
     updateDate
   } = useContext(ScrollsContext)
 
+  const [keyboardOffset, setKeyboardOffset] = useState(0);
+
   useEffect(() => {
     onChageDate(route.params.date)
     onChageText(route.params.text)
     onChageTitle(route.params.title)
+
+    const show = Keyboard.addListener('keyboardDidShow', () => setKeyboardOffset(50));
+    const hide = Keyboard.addListener('keyboardDidHide', () => setKeyboardOffset(0));
+
+    return () => {
+      show.remove();
+      hide.remove();
+    }
   }, [])
 
   const { theme } = useTheme()
   const { t } = useTranslation()
 
   return (
+    <SafeAreaView style={{ 
+      flex: 1, backgroundColor: theme.colors.background,
+    }}>
+    <KeyboardAvoidingView
+      style={{ flex: 1, paddingBottom: keyboardOffset }}
+      behavior={Platform.OS === 'ios' ? 'padding' : 'height'}>
     <View style={[styles.content, {
       backgroundColor: theme.colors.background
     }]}>
@@ -53,6 +72,8 @@ export const EditScrollScreen = ({ route }) => {
         }]}
       />
     </View>
+    </KeyboardAvoidingView>
+    </SafeAreaView>
   )
 }
 
@@ -60,6 +81,7 @@ const styles = StyleSheet.create({
   content: {
     flex: 1,
     padding: 18,
+    paddingBottom: 50
   },
   title: {
     fontSize: 18,
@@ -67,6 +89,8 @@ const styles = StyleSheet.create({
     fontWeight: 'bold'
   },
   text: {
+    flex: 1,
     fontSize: 16,
+    textAlignVertical: 'top'
   }
 });
