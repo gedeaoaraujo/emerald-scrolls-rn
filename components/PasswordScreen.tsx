f964bc711545d80d7f6e63f237329e4bc492e700
gedeoaraujo <gedeao.lemobs@gmail.com>
2025-06-25 16:24:53 -0300

    Creating password screen

---
+++ components/PasswordScreen.tsx
@@ -0,0 +1,83 @@
+import { FontAwesome6 } from '@expo/vector-icons';
+import { checkBiometry } from '../utils/biometry';
+import { PasswordKeyboard } from './PasswordKeyboard';
+import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
+import { usePasswordViewModel } from '../viewmodels/PasswordViewModel';
+import { useTheme } from '../theme/ThemeContext';
+import { useTranslation } from 'react-i18next';
+
+export const PasswordScreen = ({ navigation }) => {
+  const { 
+    password,
+    checkPassword,
+    onChangePassword 
+  } = usePasswordViewModel()
+  
+  const { theme } = useTheme()
+  const { t } = useTranslation()
+
+  const callBiometry = () => {
+    checkBiometry(t, () => {
+      navigation.replace('Home')
+    })
+  }
+
+  const startChecking = async () => {
+    if (await checkPassword()) {
+      navigation.replace('Home')
+    } else {
+      alert(t('wrong.password'))
+    }
+  }
+
+  const LogoFragment = () => (
+    <View style={styles.logo}>
+      <FontAwesome6 name='scroll' size={50} 
+        color={theme.colors.textOnPrimary}/>
+      <Text style={[styles.text, {
+        color: theme.colors.textOnPrimary
+      }]}>{t('app.name')}</Text>
+    </View>
+  )
+
+  return (
+    <View style={[styles.container, {
+      backgroundColor: theme.colors.primary
+    }]}>
+      <LogoFragment />
+      <PasswordKeyboard 
+        password={password}
+        onChangePassword={onChangePassword}
+      />
+      <View style={{ flexDirection: 'row' }}>
+        <TouchableOpacity
+          style={{ marginEnd: 40 }}
+          onPress={callBiometry}>
+          <FontAwesome6 name='fingerprint' size={50} 
+            color={theme.colors.textOnPrimary}/>
+        </TouchableOpacity>
+        <TouchableOpacity
+          onPress={startChecking}>
+          <FontAwesome6 name='circle-check' size={50} 
+            color={theme.colors.textOnPrimary}/>
+        </TouchableOpacity>
+      </View>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  logo: {
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  text: {
+    padding: 10,
+    fontSize: 30,
+  }
+})
