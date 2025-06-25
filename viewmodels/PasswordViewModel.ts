5e32c595296225930adbe0e9346394d2bfb6425b
gedeoaraujo <gedeao.lemobs@gmail.com>
2025-06-24 23:17:57 -0300

    Adding pasword methods

--- viewmodels/PasswordViewModel.ts
+++ viewmodels/PasswordViewModel.ts
@@ -1,21 +1,32 @@
 import { useState } from "react"
+import { getPassword, savePasswordConfig } from "../database/configs.dao"
 
 export function usePasswordViewModel() {
-  const savedPass: string = ''
   const [password, setPassword] = useState('')
+
+  const getSavedPass = async () => {
+    const pass = await getPassword()
+    console.log('password:', pass)
+    return pass ?? ''
+  }
+
+  const savePassword = async (pass: string) => {
+    await savePasswordConfig(pass)
+  }
 
   const onChangePassword = (str: string) => {
     setPassword(str)
   }
   
-  const checkPassword = (): boolean => {
-    const res = password === savedPass
+  const checkPassword = async (): Promise<boolean> => {
+    const res = password === await getSavedPass()
     if (res) setPassword('')
     return res
   }
 
   return {
     password,
+    savePassword,
     checkPassword,
     onChangePassword,
   }
